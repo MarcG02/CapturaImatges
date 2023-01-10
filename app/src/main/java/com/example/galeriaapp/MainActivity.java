2415edaf2731910c56bf5283c137e0ae6a6a822b
super <marcgalaxy02@gmail.com>
2023-01-10 16:47:24 +0100

    Camera Works

--- app/src/main/java/com/example/galeriaapp/MainActivity.java
+++ app/src/main/java/com/example/galeriaapp/MainActivity.java
@@ -8,40 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.galeriaapp.R;
+
 public class MainActivity extends AppCompatActivity {
     public ActivityResultLauncher<Intent> someActivityResultLauncher = null;
+    public ActivityResultLauncher<Intent> someActivityResultLauncherCamera = null;
     public static int RC_PHOTO_PICKER = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button galleryButton = findViewById(R.id.galleryButton);
+        Button cameraButton = findViewById(R.id.cameraButton);
+
+
         this.someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
-                            // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
                             ImageView imageView = findViewById(R.id.img);
@@ -55,12 +50,39 @@ public class MainActivity extends AppCompatActivity {
                 openSomeActivityForResult(null);
             }
         });
+
+
+        this.someActivityResultLauncherCamera = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Bundle extras = result.getData().getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ImageView imagen = findViewById(R.id.img);
+                            imagen.setImageBitmap(imageBitmap);
+                        }
+                    }
+                });
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSomeActivityForResult2(null);
+            }
+        });
+
     }
     public void openSomeActivityForResult(View view) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         someActivityResultLauncher.launch(intent);
+    }
+
+    public void openSomeActivityForResult2(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        someActivityResultLauncherCamera.launch(intent);
     }
 
 }
