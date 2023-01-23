6529cff98a001609e80473ef46023742593310b6
super <marcgalaxy02@gmail.com>
2023-01-23 16:14:09 +0100

    Full Size Implementation with URI

--- app/src/main/java/com/example/galeriaapp/MainActivity.java
+++ app/src/main/java/com/example/galeriaapp/MainActivity.java
@@ -1,27 +1,37 @@
 package com.example.galeriaapp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import com.example.galeriaapp.R;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    public ActivityResultLauncher<Intent> someActivityResultLauncher = null;
-    public ActivityResultLauncher<Intent> someActivityResultLauncherCamera = null;
+    public ActivityResultLauncher<Intent> galleryResultLauncher = null;
+    ActivityResultLauncher<Intent> cameraResultLauncher = null;
     public static int RC_PHOTO_PICKER = 0;
+    public static int REQUEST_IMAGE_CAPTURE = 0;
+    private Uri URIFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +39,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button galleryButton = findViewById(R.id.galleryButton);
         Button cameraButton = findViewById(R.id.cameraButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGalleryResultLauncher(null);
 
+            }
+        });
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCameraResultLauncher(null);
+            }
+        });
+        this.cameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            ImageView imageView = findViewById(R.id.img);
+                            imageView.setImageURI(URIFoto);
 
-        this.someActivityResultLauncher = registerForActivityResult(
+                        }
+                    }
+                });
+        this.galleryResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
@@ -44,45 +77,41 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-        galleryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSomeActivityForResult(null);
-            }
-        });
-
-
-        this.someActivityResultLauncherCamera = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == RESULT_OK) {
-                            Bundle extras = result.getData().getExtras();
-                            Bitmap imageBitmap = (Bitmap) extras.get("data");
-                            ImageView imagen = findViewById(R.id.img);
-                            imagen.setImageBitmap(imageBitmap);
-                        }
-                    }
-                });
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSomeActivityForResult2(null);
-            }
-        });
-
     }
-    public void openSomeActivityForResult(View view) {
+
+    public void openGalleryResultLauncher(View view) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        //Setting image type
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-        someActivityResultLauncher.launch(intent);
+        // Launching app
+        galleryResultLauncher.launch(intent);
+
     }
 
-    public void openSomeActivityForResult2(View view) {
+    public void openCameraResultLauncher(View view) {
+        //Create Intent
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        someActivityResultLauncherCamera.launch(intent);
+        //generate File dir
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File fileFoto = null;
+        try {
+            //Create temp file
+            fileFoto = File.createTempFile("foto",".jpg",storageDir);
+        } catch (IOException ex) {
+            //Error controlling the file creation
+            ex.printStackTrace();
+        }
+        if (fileFoto != null) {
+            // Getting the URIFoto
+            URIFoto = FileProvider.getUriForFile(this,
+                    "com.example.galeriaapp.fileprovider",
+                    fileFoto);
+            //Add the photo into the intent
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, URIFoto);
+            //Camera Launch
+            cameraResultLauncher.launch(intent);
+        }
     }
 
 }
